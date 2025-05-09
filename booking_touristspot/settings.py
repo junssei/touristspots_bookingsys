eb90d7e077c43ee6bb7a527179ed70a8eb2d5c37
juvncent <junevincentmagsayofernandez@gmail.com>
2025-05-09 08:55:42 +0800

    third commit

--- booking_touristspot/settings.py
+++ booking_touristspot/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3tyg+4_0=22d7oh0kxm^3vrbw$$-ij6g%a9*lfl*#_y6)^f_=a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
